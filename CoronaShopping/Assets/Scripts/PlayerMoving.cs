e19d77cda5b1235c2ad9552773ef46b61a2d0445
Kamil Zawilski <zawilskikamil@gmail.com>
2020-04-19 21:17:31 +0200

    Player animation :)

--- CoronaShopping/Assets/Scripts/PlayerMoving.cs
+++ CoronaShopping/Assets/Scripts/PlayerMoving.cs
@@ -3,28 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMoving : MovingObject
 {
-    //public InputManager inputManager = null;
+    private Animator animator;
 
-    // Use this for initialization
-    
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        base.Start();
+    }
 
     void Update()
     {
         if (InputManager.IsUp())
         {
             base.Move(0, 1);
+            animator.SetTrigger("PlayerGoUp");
         }
         else if (InputManager.IsDown())
         {
             base.Move(0, -1);
+            animator.SetTrigger("PlayerGoDown");
         }
         else if (InputManager.IsLeft())
         {
             base.Move(-1, 0);
+            animator.SetTrigger("PlayerGoLeft");
         }
         else if (InputManager.IsRight())
         {
             base.Move(1, 0);
+            animator.SetTrigger("PlayerGoRight");
+
         }
     }
 }
