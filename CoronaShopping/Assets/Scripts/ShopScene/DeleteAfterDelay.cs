8e7f05afb8fb296ec0586fe6d9c551f8cecd1c71
Kamil Zawilski <zawilskikamil@gmail.com>
2020-05-15 23:30:26 +0200

    delete Corona after 5 second

---
+++ CoronaShopping/Assets/Scripts/ShopScene/DeleteAfterDelay.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DeleteAfterDelay: MonoBehaviour
+{
+    public float delay = 5.0f;
+
+    void Start()
+    {
+        Object.Destroy(gameObject, delay);
+    }
+}
